e817a1850015506e61703d55aa43c36f9d4c7d90
Karasu090906 <jorge.goncalves15@etec.sp.gov.br>
2022-05-03 00:15:02 +0900

    trabaio

--- www/components/comandos.js
+++ www/components/comandos.js
@@ -1,2 +1,32 @@
-// This is a JavaScript file
+window.onload = function(){
+    document.querySelector("#scan").addEventListener('click', function(){
+        
+         cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          if(result.text == '1234'){
+              document.querySelector("#dados").innerHTML = "<h1> ACHEIIII </h1>
+          }else {
+              document.querySelector("#dados").innerHTML = "<h1> cade ?? </h1>"
+          }
+      },
+      function (error) {
+          alert("Falha no escaneamento , por favor tente novamente: " + error);
+      },
+      {
+          preferFrontCamera : false, 
+          showFlipCameraButton : true, 
+          showTorchButton : true, 
+          torchOn: false, 
+          saveHistory: true, 
+          prompt : "Coloque um código de barras na área de scan", 
+          resultDisplayDuration: 500, 
+          formats : "CODE_39", 
+          orientation : "landscape",  
+          disableAnimations : true, 
+          disableSuccessBeep: true
+      }
+   );
 
+    });
+
+}    
